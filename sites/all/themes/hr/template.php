fa5d7ea92952e5ce1555590226ed08efbf87d0b3
helgacheberakha <helga.cheberakha@dev-pro.net>
2015-12-08 00:56:21 +0200

    add menu block

--- sites/all/themes/hr/template.php
+++ sites/all/themes/hr/template.php
@@ -3,3 +3,6 @@
  * @file
  * The primary PHP file for this theme.
  */
+function hr_menu_tree__menu_administration_menu($variables) {
+  return '<ul class="nav nav-pills">' . $variables['tree'] . '</ul>';
+}
