675dcb2ad8b9374f451e5e783b6538c57bfd03dc
helgacheberakha <helga.cheberakha@dev-pro.net>
2015-12-12 15:22:01 +0200

    added some changes - send email with attachment, add new styles

--- sites/all/modules/custom/hr_surveys/js/hr_term_reference_tree.js
+++ sites/all/modules/custom/hr_surveys/js/hr_term_reference_tree.js
@@ -11,55 +11,37 @@
         $(this).find('input.form-checkbox').each(function() {
           var parent = $(this);
           // Find if isset subterms
-          var children_block = $(this).parent().parent().next('ul.term-reference-tree-level');
+          var child_block = $(this).parent().parent().next('ul.term-reference-tree-level');
 
-          if (children_block.length > 0) {
-            var children = children_block.find('input.form-checkbox');
-
+          if (child_block.length > 0) {
             // Show subterms if parent element si checked
             if ($(this).attr('checked')) {
-              children_block.show();
+              child_block.show();
             }
-
-            // Disable parent element if at least one subterm is checked
-            children.each(function() {
-              if ($(this).is(':checked')) {
-                parent.attr('disabled', 'disabled');
-                checked++;
-              }
-            });
-
-            // Enable parent element if all subterms aren't checked
-            children.change(function(event) {
-              if($(this).is(':checked')) {
-                checked++;
-              }
-              else {
-                checked--;
-              }
-
-              if (checked > 0) {
-                parent.attr('disabled', 'disabled');
-              }
-              else {
-                parent.attr('disabled', false);
-              }
-            });
-
           }
         });
 
         // Show/hide subterms
         $(this).find('input.form-checkbox').change(function(event) {
           var event_target = $(event.target);
-          var children = event_target.parent().parent().next('ul.term-reference-tree-level');
-          if(event_target.filter(':checked').length) {
-            children.show();
-          }
-          else {
-            children.hide();
+          var child_block = event_target.parent().parent().next('ul.term-reference-tree-level');
+
+          if (child_block.length > 0) {
+            var children = child_block.find('input.form-checkbox');
+
+            if(event_target.filter(':checked').length) {
+              child_block.show();
+            }
+            else {
+              child_block.hide();
+              // Unchecked child elements when parent element is unchecked
+              children.each(function() {
+                $(this).prop('checked', false);
+              });
+            }
           }
         });
+
       });
     }
   };
